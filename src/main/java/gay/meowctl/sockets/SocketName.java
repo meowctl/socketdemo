8016e3da9500067636cc1663607a11e8905384ba
lulu <meowctl@proton.me>
2025-02-14 20:45:54 -0300

    switch to timestamp based names

--- src/main/java/gay/meowctl/sockets/SocketName.java
+++ src/main/java/gay/meowctl/sockets/SocketName.java
@@ -1,29 +1,28 @@
 package gay.meowctl.sockets;
 
 import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SocketName {
-    private static final String PREFIX = "gb-";
-    private static final SecureRandom RNG = new SecureRandom();
+    private static final String PREFIX = "gbru-";
+    private static final int TIMESTAMP_BITS = 41;
+    private static final int COUNTER_BITS = 19;
+    private static final long TIMESTAMP_MAX_VALUE = (1L << TIMESTAMP_BITS) - 1;
+    private static final int COUNTER_MAX_VALUE = (1 << COUNTER_BITS) - 1;
 
     private final long pid = ProcessHandle.current().pid();
     private final AtomicInteger counter = new AtomicInteger();
 
     public String nextName() {
-        // pid (1~64bit) + counter (32bit) + random (8bit)
+        long millis = System.currentTimeMillis() & TIMESTAMP_MAX_VALUE;
+        int counterValue = counter.getAndIncrement() & COUNTER_MAX_VALUE;
+
+        // pid (1~64bit) + timestamp + counter
         BigInteger socketId = BigInteger
-                .valueOf(pidOrRandom())
-                .shiftLeft(32)
-                .or(BigInteger.valueOf(counter.getAndIncrement()))
-                .shiftLeft(8)
-                .or(BigInteger.valueOf(RNG.nextInt(1<<8)));
+                .valueOf(pid)
+                .shiftLeft(TIMESTAMP_BITS + COUNTER_BITS)
+                .or(BigInteger.valueOf(millis << COUNTER_BITS | counterValue));
 
         return PREFIX + socketId.toString(36);
-    }
-
-    public long pidOrRandom() {
-        return pid != 0 ? pid : RNG.nextLong(1, Long.MAX_VALUE);
     }
 }
